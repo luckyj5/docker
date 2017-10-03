ee54102a956e39f02899a5c88990bebe4e4c1639
Shubham Jain <luckyj5@users.noreply.github.com>
2017-10-03 15:23:08 -0700

    replaced hard-coded values with ENV variables

--- kafka-connect-splunk-sink/script.sh
+++ kafka-connect-splunk-sink/script.sh
@@ -1,21 +1,10 @@
 #!/bin/sh
-/opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/zookeeper-server-start.sh -daemon /opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/config/zookeeper.properties
-sleep 5
-/opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/kafka-server-start.sh -daemon /opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/config/server.properties
-sleep 15
-/opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 10 --topic offset.storage.topic
-/opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 10 --topic config.storage.topic
-/opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 10 --topic status.storage.topic
-/opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 10 --topic perf
+#Start kafka-connect
+sleep 30
+/opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/connect-distributed.sh -daemon /kafka-connect-splunk/connect-distributed.properties
 
-sleep 5
-
-java -jar ./kafka-data-gen.jar -message-count 1000000 -message-size 256 -message-delay 0 -output-file "./logfile.txt" -topic perf -bootstrap.servers "localhost:9092" -acks all -kafka-retries 0 -kafka-batch-size 16384 -kafka-linger 1 -kafka-buffer-memory 33554432
-
-
-/opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/connect-distributed.sh /kafka-connect-splunk/connect-distributed.properties
-echo "starting kafka-connect"
-
-curl localhost:8083/connectors -X POST -H "Content-Type: application/json" -d '{"name": "kafka-connect-splunk","config":{"topics": "perf","tasks.max": "3","connector.class": "com.splunk.kafka.connect.SplunkSinkConnector","splunk.hec.uri": "https://127.0.0.1:8088","splunk.hec.token": "842637D8-C86E-4915-8976-D6857B5407C1","splunk.hec.raw": "false","splunk.hec.ack.enabled": "true","splunk.hec.ssl.validate.certs": "false","name": "kafka-connect-splunk"}}}'
+#Start kafka-connect-splunk Sink connector
+sleep 30
+curl kafka-connect:8083/connectors -X POST -H "Content-Type: application/json" -d '{"name": "kafka-connect-splunk","config":{"topics": "$KAFKA_TOPIC","tasks.max": "3","connector.class": "com.splunk.kafka.connect.SplunkSinkConnector","splunk.hec.uri": "$SPLUNK_HEC_ENDPOINT","splunk.hec.token": "$SPLUNK_HEC_TOKEN","splunk.hec.raw": "false","splunk.hec.ack.enabled": "true","splunk.hec.ssl.validate.certs": "false","name": "kafka-connect-splunk"}}}'
 
 tail -f /dev/null
--- kafka-data-gen/script.sh
+++ kafka-data-gen/script.sh
@@ -5,5 +5,5 @@ sleep 60
 /opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 10 --topic status.storage.topic
 /opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 10 --topic perf
 sleep 20
-java -jar ./kafka-data-gen.jar -message-count 1000000 -message-size 256 -message-delay 0 -output-file "./logfile.txt" -topic perf -bootstrap.servers "kafka:9092" -acks all -kafka-retries 0 -kafka-batch-size 16384 -kafka-linger 1 -kafka-buffer-memory 33554432
+java -jar ./kafka-data-gen.jar -message-count $KAFKA_MSG_COUNT -message-size $KAFKA_MSG_SIZE -message-delay 0 -output-file "./logfile.txt" -topic $KAFKA_TOPIC -bootstrap.servers "kafka:9092" -acks all -kafka-retries 0 -kafka-batch-size $KAFKA_BATCH_SIZE -kafka-linger 1 -kafka-buffer-memory $KAFKA_BUFFER_MEMORY
 tail -f /dev/null
