8f56dd7e3835c79c39e1e7d02c64870ba8e4068e
Shubham Jain <luckyj5@users.noreply.github.com>
2017-10-03 18:10:23 -0700

    modified env variables to adjust quotes to work with curl

--- kafka-connect-splunk-sink/script.sh
+++ kafka-connect-splunk-sink/script.sh
@@ -5,6 +5,6 @@ sleep 30
 
 #Start kafka-connect-splunk Sink connector
 sleep 30
-curl kafka-connect:8083/connectors -X POST -H "Content-Type: application/json" -d '{"name": "kafka-connect-splunk","config":{"topics": "$KAFKA_TOPIC","tasks.max": "3","connector.class": "com.splunk.kafka.connect.SplunkSinkConnector","splunk.hec.uri": "$SPLUNK_HEC_ENDPOINT","splunk.hec.token": "$SPLUNK_HEC_TOKEN","splunk.hec.raw": "false","splunk.hec.ack.enabled": "true","splunk.hec.ssl.validate.certs": "false","name": "kafka-connect-splunk"}}}'
+curl kafka-connect:8083/connectors -X POST -H "Content-Type: application/json" -d '{"name": "kafka-connect-splunk","config":{"topics": "'"$KAFKA_TOPIC"'","tasks.max": "3","connector.class": "com.splunk.kafka.connect.SplunkSinkConnector","splunk.hec.uri": "'"$SPLUNK_HEC_ENDPOINT"'","splunk.hec.token": "'"$SPLUNK_HEC_TOKEN"'","splunk.hec.raw": "false","splunk.hec.ack.enabled": "true","splunk.hec.ssl.validate.certs": "false","name": "kafka-connect-splunk"}}}'
 
 tail -f /dev/null
