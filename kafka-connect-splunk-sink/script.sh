d079bd7d11ecdf6df356ee4652151cd9f95328cd
Shubham Jain <luckyj5@users.noreply.github.com>
2017-10-06 13:25:08 -0700

    Updated script to delay connector task execution for dependency reasons

--- kafka-connect-splunk-sink/script.sh
+++ kafka-connect-splunk-sink/script.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #Start kafka-connect
-sleep 30
+sleep 60
 /opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/connect-distributed.sh -daemon /kafka-connect-splunk/connect-distributed.properties
 
 #Start kafka-connect-splunk Sink connector
-sleep 30
+sleep 360
 curl kafka-connect:8083/connectors -X POST -H "Content-Type: application/json" -d '{"name": "kafka-connect-splunk","config":{"topics": "'"$KAFKA_TOPIC"'","tasks.max": "3","connector.class": "com.splunk.kafka.connect.SplunkSinkConnector","splunk.hec.uri": "'"$SPLUNK_HEC_ENDPOINT"'","splunk.hec.token": "'"$SPLUNK_HEC_TOKEN"'","splunk.hec.raw": "false","splunk.hec.ack.enabled": "true","splunk.hec.ssl.validate.certs": "false","name": "kafka-connect-splunk"}}}'
 
 tail -f /dev/null
