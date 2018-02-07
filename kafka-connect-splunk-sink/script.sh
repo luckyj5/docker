361d68da9a28161933b9451d3eb04552064514ab
Shubham Jain <luckyj5@users.noreply.github.com>
2018-02-06 17:34:50 -0800

    Updated Dockerfile and script to include more config params

--- kafka-connect-splunk-sink/script.sh
+++ kafka-connect-splunk-sink/script.sh
@@ -4,7 +4,23 @@ sleep 60
 /opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/connect-distributed.sh -daemon /kafka-connect-splunk/connect-distributed.properties
 
 #Start kafka-connect-splunk Sink connector
-sleep 360
-curl kafka-connect:8083/connectors -X POST -H "Content-Type: application/json" -d '{"name": "kafka-connect-splunk","config":{"topics": "'"$KAFKA_TOPIC"'","tasks.max": "3","connector.class": "com.splunk.kafka.connect.SplunkSinkConnector","splunk.hec.uri": "'"$SPLUNK_HEC_ENDPOINT"'","splunk.hec.token": "'"$SPLUNK_HEC_TOKEN"'","splunk.hec.raw": "false","splunk.hec.ack.enabled": "true","splunk.hec.ssl.validate.certs": "false","name": "kafka-connect-splunk"}}}'
+sleep 60
+curl kafka-connect:8083/connectors -X POST -H "Content-Type: application/json" -d '{
+	"name": "'"$CONNECTOR_NAME"'",
+	"config":{
+		"topics": "'"$KAFKA_TOPIC"'",
+		"tasks.max": "'"$MAX_TASKS"'",
+		"connector.class": "com.splunk.kafka.connect.SplunkSinkConnector",
+		"splunk.hec.uri": "'"$SPLUNK_HEC_ENDPOINT"'",
+		"splunk.hec.token": "'"$SPLUNK_HEC_TOKEN"'",
+		"splunk.hec.raw": "'"$SPLUNK_HEC_RAW"'",
+		"splunk.hec.ack.enabled": "'"$SPLUNK_HEC_ACK_ENABLED"'",
+		"splunk.hec.ssl.validate.certs": "false",
+    "splunk.indexes": "'"$SPLUNK_INDEX"'",
+    "splunk.sources": "'"$SPLUNK_SOURCE"'",
+    "splunk.sourcetypes": "'"$SPLUNK_SOURCETYPE"'",
+		"name": "kafka-connect-splunk"
+	}
+}'
 
 tail -f /dev/null
