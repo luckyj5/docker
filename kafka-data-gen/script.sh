39680a1e9d5cdba08d91df489f7c15062141da08
Shubham Jain <luckyj5@users.noreply.github.com>
2018-02-06 17:19:00 -0800

    Updated dockerfile and script to include more config params

--- kafka-data-gen/script.sh
+++ kafka-data-gen/script.sh
@@ -3,7 +3,10 @@ sleep 60
 /opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 10 --topic offset.storage.topic
 /opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 10 --topic config.storage.topic
 /opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 10 --topic status.storage.topic
-/opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 10 --topic perf
+/opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 10 --topic $KAFKA_TOPIC
 sleep 20
-java -jar ./kafka-data-gen.jar -message-count $KAFKA_MSG_COUNT -message-size $KAFKA_MSG_SIZE -message-delay 0 -output-file "./logfile.txt" -topic $KAFKA_TOPIC -bootstrap.servers "kafka:9092" -acks all -kafka-retries 0 -kafka-batch-size $KAFKA_BATCH_SIZE -kafka-linger 1 -kafka-buffer-memory $KAFKA_BUFFER_MEMORY
+
+java -jar ./kafka-data-gen.jar -message-count $KAFKA_MSG_COUNT -message-size $KAFKA_MSG_SIZE -topic $KAFKA_TOPIC -bootstrap.servers "kafka:9092" -acks all -kafka-retries 0 -kafka-batch-size $KAFKA_BATCH_SIZE -kafka-linger 1 -kafka-buffer-memory $KAFKA_BUFFER_MEMORY -eps $KAFKA_EPS
+
+
 tail -f /dev/null
